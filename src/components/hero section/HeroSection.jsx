d036cd8c2038a5e7cb203732f33cda943617fcd0
Sandesh Dulawat <sdulawat9174799616@gmail.com>
2023-12-05 00:16:00 +0530

    final update

--- src/components/hero section/HeroSection.jsx
+++ src/components/hero section/HeroSection.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function HeroSection() {
   return (
     <div className="bg-[#111827] ">
-      <div className="max-w-screen-lg  md:max-w-screen-xl mx-auto w-full h-full px-8 py-16 flex flex-col-reverse sm:flex-row sm:justify-between md:flex-row md:justify-between text-white items-center">
+      <div className="max-w-screen-lg md:max-w-screen-xl mx-auto w-full h-full px-8 py-16 md:py-32 flex flex-col-reverse sm:flex-row sm:justify-between md:flex-row md:justify-between text-white items-center">
         <div className="space-y-2 sm:space-y-2 md:space-y-5">
           <h1 className="text-white font-bold text-3xl sm:text-4xl md:text-5xl">
             Hi, I am <span className="text-blue-500">Sandesh.</span>{" "}
@@ -14,9 +14,8 @@ function HeroSection() {
           <h2 className="text-white text-lg sm:text-md lg:text-lg lg:text-lg">
             Front-end Developer
           </h2>
-          <h3 className="text-gray-400 text-sm sm:text-sm lg:text-md lg:text-md">
-            I created stunning websites for your bisuness, highly experienced in
-            web design and development.
+          <h3 className="text-gray-400 text-sm sm:text-sm lg:text-md lg:text-md md:tracking-wide leading-4">
+          I specialize in crafting stunning websites for your business, bringing a wealth of experience in <br/> web design and development. With a keen eye for detail and a commitment to excellence, I ensure that <br/> your online presence not only looks impressive but also functions <br/> seamlessly to enhance your brand.
           </h3>
           <div className="flex space-x-5 text-blue-500 py-10">
             <Link to="https://github.com/sandeshdulawat">
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,9 @@ function Navbar() {
   return (
     
 
-<nav className="bg-[#111827] fixed w-full">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
+<nav className="bg-[#111827] sticky top-0 w-full ">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-8 ">
     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
         <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Portfolio</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden " aria-controls="navbar-default" aria-expanded="false">
@@ -18,22 +17,22 @@ function Navbar() {
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
-    <div className="hidden w-full md:block md:w-auto " id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 ">
+    <div className="hidden w-full md:block md:w-auto bg-[#111827] " id="navbar-default">
+      <ul className="bg-[#111827] font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 ">
         <li>
-          <a href="#" className="block py-2 px-3 text-white bg-[#111827] rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
+          <a href="#" className="block py-2 px-3 text-white bg-[#111827] text-white md:text-blue-500 md:p-0 " aria-current="page">Home</a>
         </li>
         <li>
-          <a href="#about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">About</a>
+          <a href="#about" className="block py-2 px-3 text-gray-900 text-white md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">About</a>
         </li>
         <li>
-          <a href="#skills" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Skills</a>
+          <a href="#skills" className="block py-2 px-3 text-gray-900 text-white md:hover:text-blue-700 md:p-0 ">Skills</a>
         </li>
         <li>
-          <a href="#projects" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Projects</a>
+          <a href="#projects" className="block py-2 px-3 text-gray-900 text-white md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Projects</a>
         </li>
         <li>
-          <a href="#footercontact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Contact</a>
+          <a href="#footercontact" className="block py-2 px-3 text-gray-900 text-white md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Contact</a>
         </li>
       </ul>
     </div>
