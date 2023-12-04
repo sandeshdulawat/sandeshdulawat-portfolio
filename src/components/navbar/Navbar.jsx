4a6d5984aaced11d08cab83b15ad44ed339a0e67
Sandesh Dulawat <sdulawat9174799616@gmail.com>
2023-12-05 00:24:36 +0530

    final update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     
 
 <nav className="bg-[#111827] sticky top-0 w-full ">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-8 ">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
         <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Portfolio</span>
     </a>
