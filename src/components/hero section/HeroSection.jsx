4c44e78bff250057defec16a258b0e66ba460321
Sandesh Dulawat <sdulawat9174799616@gmail.com>
2023-12-05 00:21:18 +0530

    final update

--- src/components/hero section/HeroSection.jsx
+++ src/components/hero section/HeroSection.jsx
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className="bg-[#111827] ">
       <div className="max-w-screen-lg md:max-w-screen-xl mx-auto w-full h-full px-8 py-16 md:py-32 flex flex-col-reverse sm:flex-row sm:justify-between md:flex-row md:justify-between text-white items-center">
-        <div className="space-y-2 sm:space-y-2 md:space-y-5">
+        <div className="space-y-2 sm:space-y-2 md:space-y-5 md:w-7/12">
           <h1 className="text-white font-bold text-3xl sm:text-4xl md:text-5xl">
             Hi, I am <span className="text-blue-500">Sandesh.</span>{" "}
           </h1>
@@ -15,7 +15,7 @@ function HeroSection() {
             Front-end Developer
           </h2>
           <h3 className="text-gray-400 text-sm sm:text-sm lg:text-md lg:text-md md:tracking-wide leading-4">
-          I specialize in crafting stunning websites for your business, bringing a wealth of experience in <br/> web design and development. With a keen eye for detail and a commitment to excellence, I ensure that <br/> your online presence not only looks impressive but also functions <br/> seamlessly to enhance your brand.
+          I specialize in crafting stunning websites for your business, bringing a wealth of experience in web design and development. With a keen eye for detail and a commitment to excellence, I ensure that your online presence not only looks impressive but also functions seamlessly to enhance your brand.
           </h3>
           <div className="flex space-x-5 text-blue-500 py-10">
             <Link to="https://github.com/sandeshdulawat">
