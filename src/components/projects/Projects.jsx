b24ee5d5f36cd8b94a89753500e12efb2e0f2842
Sandesh Dulawat <sdulawat9174799616@gmail.com>
2024-07-16 01:23:58 +0530

    green energy project added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,6 +4,7 @@ import mazeStudio from "../../assets/images/mazestudio.webp";
 import archazine from "../../assets/images/archazine-fullpage.webp";
 import lodha from "../../assets/images/lodhabellevues-fullpage.webp";
 import isanction from "../../assets/images/isanction.webp";
+import GreenEnergy from "../../assets/images/green.png";
 
 function Projects() {
   return (
@@ -110,7 +111,7 @@ function Projects() {
                   Lodha Bellevue
                 </h5>
                 <h5 className="mb-2 md:text-md tracking-tight  text-white ">
-                  Real-state Website
+                  Real estate Website
                 </h5>
               </div>
               <p className="mb-3 md:text-sm text-gray-400">
@@ -143,6 +144,84 @@ function Projects() {
                 </a>
                 <a
                   href="https://github.com/sandeshdulawat/project-1001-react"
+                  target="blank_"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Source Code
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 30 30"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </a>
+          <div className="absolute top-0 right-0">
+            <span class="flex text-lg md:flex items-center text-red-700 font-bold uppercase">
+              <span class="w-3 h-3 mr-3 relative live_icon"></span> Live
+            </span>
+          </div>
+        </div>
+
+        {/* green energy project  */}
+        <div className="flex flex-col items-center px-5 md:px-0 relative">
+          <a className="flex flex-col rounded-lg shadow md:flex-row  bg-secondary my-10">
+            <div className="w-full h-72 overflow-hidden rounded-lg">
+              <img
+                className="hover:animate-scrollDown bg-cover"
+                src={GreenEnergy}
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col justify-between px-4 leading-normal bg-secondary space-y-5 rounded-lg py-5">
+              <div>
+                <h5 className="mb-2 text-xl md:text-2xl lg:text-3xl font-bold tracking-tight  text-white ">
+                  Arch - The Future of Green Energy
+                </h5>
+                <h5 className="mb-2 md:text-md tracking-tight  text-white ">
+                  Environmental Education and Awareness
+                </h5>
+              </div>
+              <p className="mb-3 md:text-sm text-gray-400">
+                Complete website for a Environmental Education and Awareness. This website is built
+                with NextJS, Node.js, Express.js, MongoDB, Tailwind CSS.
+                The website is responsive and has a
+                clean & modern design.
+              </p>
+              <div className="space-x-3">
+                <a
+                  href="https://project-green-energy-nextjs-14.netlify.app"
+                  target="blank_"
+                  className="inline-flex items-center px-2 py-2 md:px-3 md:py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 align-center"
+                >
+                  Demo
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 32 32"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                  </svg>
+                </a>
+                <a
+                  href="https://github.com/sandeshdulawat/project-green-energy-nextjs-14.git"
                   target="blank_"
                   className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
@@ -277,7 +356,8 @@ function Projects() {
               </p>
               <div className="space-x-3">
                 <a
-                  href="https://www.mazestudio.in/" target="blank_"
+                  href="https://www.mazestudio.in/"
+                  target="blank_"
                   className="inline-flex items-center px-2 py-2 md:px-3 md:py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 align-center"
                 >
                   Demo
@@ -394,8 +474,6 @@ function Projects() {
             </div>
           </a>
         </div>
-
-
       </div>
     </div>
   );
